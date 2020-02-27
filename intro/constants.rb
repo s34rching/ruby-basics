32b1ec8231ec026d37753b16a73ebfe913e1ac00
s34rching <ivan.katsenya@gmail.com>
2020-02-27 11:32:56 +0300

    Constants

---
+++ intro/constants.rb
@@ -0,0 +1,25 @@
+# Constants
+
+age = 25
+puts age
+
+age = 30
+puts age
+
+# Interpreter throws warning, but allows to override variable value
+
+DEVICE_NAME = "Display"
+puts DEVICE_NAME
+
+DEVICE_NAME = "Table"
+puts DEVICE_NAME
+
+# Variable starting with capital letter is treated as constant as well
+
+Version = "10.5"
+puts Version
+
+Version = "11.3"
+puts Version
+
+
