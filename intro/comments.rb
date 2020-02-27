a018c0fd240470e0655fd00553ec8c33645811ec
s34rching <ivan.katsenya@gmail.com>
2020-02-27 11:00:13 +0300

    Commenting code

---
+++ intro/comments.rb
@@ -0,0 +1,17 @@
+# Single line comment
+
+puts "Hello World!" # Another comment
+
+# This
+# is
+# a
+# multiline
+# comment
+
+
+=begin
+
+puts "This is also multiline comment"
+puts "Bad practice comment"
+
+=end
