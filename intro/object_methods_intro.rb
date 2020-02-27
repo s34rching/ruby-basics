d376bd99185a73d84f4058bc3ae3f8aacafd8186
s34rching <ivan.katsenya@gmail.com>
2020-02-27 11:41:49 +0300

    Object Methods Intro

---
+++ intro/object_methods_intro.rb
@@ -0,0 +1,13 @@
+# Intro to Object Methods
+
+puts "string".length
+puts "string".upcase
+puts "STRING".downcase
+
+foo = "String"
+puts foo.length
+
+puts 10.next
+puts -1.next
+
+puts "Hello World".inspect # == p "Hello World"
