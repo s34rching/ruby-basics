507bc15c12884631bf7328c3cc433618d6b82f78
s34rching <ivan.katsenya@gmail.com>
2020-02-27 10:43:36 +0300

    Basic arithmetic

---
+++ intro/basic_arithmetic.rb
@@ -0,0 +1,31 @@
+# Addition
+
+p 5 + 55
+p 1 + 0.524
+
+# Subtraction
+
+p 5 - 55
+
+# Multiplication
+
+p 4 * 9
+p "Mac" + "Mini"
+
+# Division
+
+p 10 / 5
+p 12 / 5
+p 12.0 / 5
+p 12 / 5.0
+p 12.0 / 5.0
+p 10 / 0.5
+
+# Power of
+
+p 8 ** 2
+
+# Modulus
+
+p 7 % 4
+p 8 % 4
