94d4c9926754378676edda2904b7d1046ed4e6e2
s34rching <ivan.katsenya@gmail.com>
2020-03-01 15:40:37 +0300

    Multiline string

---
+++ s4-strings/multiline_string.rb
@@ -0,0 +1,12 @@
+# Multiline string
+
+# identifier 'MLS' can be whatever
+multilines = <<MLS
+    This is a string
+    having multiple lines.
+
+    Syntax is pretty
+    confusing for doing that
+MLS
+
+puts multilines
