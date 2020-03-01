1a42dc904f01f264152acef9e247b97579fc67d5
s34rching <ivan.katsenya@gmail.com>
2020-03-01 16:41:24 +0300

    Concatenation

---
+++ s4-strings/concatenation.rb
@@ -0,0 +1,28 @@
+# Strings concatenation
+
+first_name = "Bobby"
+last_name = "Roy"
+
+# '+' sign
+# person = first_name + " " + last_name
+# puts person
+
+# '+='
+# person1 = first_name += " #{last_name}"
+# puts first_name # mutated
+# puts person1
+
+# concat()
+# person2 = first_name.concat(" ").concat(last_name)
+# puts first_name # mutated
+# puts person2
+
+# '<<'
+# person3 = first_name << " " << last_name
+# puts first_name # mutated
+# puts person3
+
+# prepend()
+# person4 = last_name.prepend(" ").prepend(first_name)
+# puts last_name # mutated
+# puts person4
