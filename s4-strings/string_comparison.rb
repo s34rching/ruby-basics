b4d2352939dfc542ff641c02f84fb86b9b9c7cbd
s34rching <ivan.katsenya@gmail.com>
2020-03-01 16:28:12 +0300

    Strings: comparison

---
+++ s4-strings/string_comparison.rb
@@ -0,0 +1,16 @@
+# Strings equality and inequality
+
+a = "hello"
+b = "Hello"
+c = "hello"
+
+puts a == c
+puts a != b
+puts a == b.downcase
+
+# Alphabetical sort
+
+puts "A" < "B"
+puts "Apple" < "Alphabetical"
+puts "A" < "a"
+puts "Z" < "a"
