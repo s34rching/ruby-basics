dee70f6166f90a46657a8d4425e47cad9f75b6a4
s34rching <ivan.katsenya@gmail.com>
2020-03-03 11:23:53 +0300

    Case methods

---
+++ s4-strings/case_methods.rb
@@ -0,0 +1,21 @@
+# String case methods
+
+p "hello".capitalize
+p "heLLO".capitalize # downcase other letters
+p "Hello".capitalize
+p "hello world".capitalize # capitalize only the first letter
+
+p "hello123".upcase
+p "heLLO".upcase
+p "Hello!@#".upcase
+p "hello world".upcase
+
+p "hello1$".downcase
+p "heLLO".downcase
+p "Hello".downcase
+p "HELLO WORLD".downcase
+
+p "hello1$".swapcase
+p "heLLO".swapcase
+p "Hello".swapcase
+p "HELLO WORLD".swapcase
