1ee5b7bd3b4691dd319246b4ba8349c2e333d2d8
s34rching <ivan.katsenya@gmail.com>
2020-03-03 11:37:45 +0300

    Bang methods

---
+++ s4-strings/bang_methods.rb
@@ -0,0 +1,11 @@
+# Bang methods - mutates original object
+
+a = "hello"
+a.capitalize! # same as a = a.capitalize
+p a
+
+a.upcase! # same as a = a.upcase
+p a
+
+a.reverse! # same as a = a.reverse
+p a
