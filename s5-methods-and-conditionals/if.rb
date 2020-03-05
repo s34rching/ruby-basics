ea8680964b147be2059ef42d52c7a1c592302c94
s34rching <ivan.katsenya@gmail.com>
2020-03-05 18:15:01 +0300

    If

---
+++ s5-methods-and-conditionals/if.rb
@@ -0,0 +1,25 @@
+# If
+
+if 2 > 1
+  puts "That math statement is true!"
+end
+
+if 5
+  puts "It works"
+end
+
+if "string"
+  puts "It also works!"
+end
+
+if 0
+  puts "Ooops, it works!"
+end
+
+if false
+  puts "It does not 'puts'"
+end
+
+if nil
+  puts "It does not work as well"
+end
