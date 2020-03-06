d1e926695246ab53e625d9d5b8f4fe4df5490c11
s34rching <ivan.katsenya@gmail.com>
2020-03-06 11:41:13 +0300

    Elsif / else

---
+++ s5-methods-and-conditionals/elsif_else.rb
@@ -0,0 +1,13 @@
+# Elsif / else
+
+color = "t"
+
+if color == "green"
+  puts "It's green"
+elsif color == "red"
+  puts "It's red"
+elsif color == "yellow"
+  puts "It's yellow"
+else
+  puts "Is it transparent?"
+end
