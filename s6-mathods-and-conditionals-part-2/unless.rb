dfda88dd57d9d075d9d2ad88c1609fa46972995c
s34rching <ivan.katsenya@gmail.com>
2020-03-08 16:58:45 +0300

    Unless

---
+++ s6-mathods-and-conditionals-part-2/unless.rb
@@ -0,0 +1,15 @@
+# unless
+
+password = "topsecret"
+
+if password != "topsecret"
+  "Not allowed"
+else
+  "U r welcome"
+end
+
+unless password == "topsecret"
+  "It is not allowed"
+else
+  "You are welcome"
+end
