525a8b7b032f3d602e160daa1f79c8679252a4f3
s34rching <ivan.katsenya@gmail.com>
2020-03-10 11:02:52 +0300

    Intro

---
+++ s7-ranges/intro.rb
@@ -0,0 +1,17 @@
+# Introduction
+
+numbers = 5..20 # Including max
+nums = 5...20 # Excluding max
+p numbers
+p numbers.class # Range
+
+p nums.first
+p numbers.first
+p numbers.first(1) # returns first element in array
+p numbers.first(5) # returns array of 5 first elements of Range
+p nums.first(5) # returns array of 5 first elements of Range
+p nums.last # 20 - designed to show 20 despite max is excluded
+p numbers.last
+p numbers.last(1) # returns the last element in array
+p numbers.last(10) # returns array of 10 first elements of Range
+p nums.last(10) # returns array of 10 first elements of Range
