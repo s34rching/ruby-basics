07823ef49c6b0a836d42bb212c9f62b5798dd9bc
s34rching <ivan.katsenya@gmail.com>
2020-03-13 22:55:45 +0300

    reverse!

--- s9-arrays-iteration-and-variuos-methods.rb/reverse.rb
+++ s9-arrays-iteration-and-variuos-methods.rb/reverse.rb
@@ -1,7 +1,15 @@
 nums = (5..15).to_a
 
+p nums
 p nums.reverse
 
 strs = %[Ionic cannon at the holodeck was the death of nuclear flux destroyed to a mysterious processor]
 
+p strs
 p strs.reverse
+
+booleans = [ true, false, false, false ]
+
+p booleans
+p booleans.reverse!
+p booleans
