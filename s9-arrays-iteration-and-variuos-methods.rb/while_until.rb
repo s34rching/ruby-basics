ba414d85b9ac892d0b011b4137205033080d72fc
s34rching <ivan.katsenya@gmail.com>
2020-03-13 22:35:01 +0300

    while_until_loops.rb

---
+++ s9-arrays-iteration-and-variuos-methods.rb/while_until.rb
@@ -0,0 +1,18 @@
+# while
+
+str = %w[Advice at the planet was the beauty of mineral fighted to a crazy nanomachine]
+
+i = 0
+while i < str.length
+  p str[i]
+  i += 1
+end
+
+# until
+
+j = 0
+until j == str.length
+  p str[j]
+  j += 1
+end
+
