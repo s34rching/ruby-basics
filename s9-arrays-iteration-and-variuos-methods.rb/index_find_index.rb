35f00b31fc92f99ec4099231a0b3dec4ce3e0a31
s34rching <ivan.katsenya@gmail.com>
2020-03-14 23:00:12 +0300

    index find_index methods

---
+++ s9-arrays-iteration-and-variuos-methods.rb/index_find_index.rb
@@ -0,0 +1,8 @@
+colors = ["Red", "Green", "Blue", "Black", "Black"]
+
+p colors.index("Red")
+p colors.find_index("Red")
+p colors.index("Black") # returns index of the first occurrence
+p colors.find_index("Black") # returns index of the first occurrence
+p colors.index("Orange") # nil
+p colors.find_index("Orange") # nil
