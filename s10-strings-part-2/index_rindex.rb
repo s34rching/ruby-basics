12af88c11def4bd8fed0c73ee925413bf9fb4767
s34rching <ivan.katsenya@gmail.com>
2020-03-15 16:52:53 +0300

    'index' and 'rindex' methods

---
+++ s10-strings-part-2/index_rindex.rb
@@ -0,0 +1,31 @@
+str = "Yo-ho-ho, cloudy pestilence"
+
+p str.index("Y") # => 0
+p str.index("o") # => 1 index of first of the match
+p str.index("ho") # => 3 index of first character of the match
+p str.index("o", 9) # => 12
+
+# rindex - reverse index
+
+p str.downcase.rindex("y") # => 15
+p str.rindex("o") # => 12 index of first of the match
+p str.rindex("ho") # => 6 index of first character of the match
+p str.rindex("o", 9) # => 7
+
+# Exercise
+
+def get_custom_index(str, match)
+  match_index = 0
+
+  if str.include?(match)
+    str.chars.each_with_index do |char, index|
+      return match_index = index if str[index, match.length] == match
+    end
+  end
+  return nil
+end
+
+p get_custom_index(str, "Y")
+p get_custom_index(str, "o")
+p get_custom_index(str, "z")
+p get_custom_index(str, "cl")
