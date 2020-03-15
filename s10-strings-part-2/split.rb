c4c27fd71a4f4ca05788bee600bd335e9734367c
s34rching <ivan.katsenya@gmail.com>
2020-03-15 12:02:35 +0300

    'split' exercise

--- s10-strings-part-2/split.rb
+++ s10-strings-part-2/split.rb
@@ -7,3 +7,22 @@ arr2 = str.split(" in")
 p arr
 p arr1
 p arr2
+
+# Exercise
+
+def get_longest(str)
+  words = str.split
+  mapped = words.map { |word| word.length }
+  max_length = mapped.max
+
+  words_with_max_length = words.reject { |word| word.length < max_length }
+  words_with_max_length.length == 1 ? words_with_max_length[0] : words_with_max_length.last
+end
+
+test_str = "Zeluss nocere in caelos"
+test_str1 = "Unconditional emptinesses synthesises most faiths"
+test_str2 = "Try heating margerine casserole seasoned with meatballs juice"
+
+p get_longest(test_str)
+p get_longest(test_str1)
+p get_longest(test_str2)
