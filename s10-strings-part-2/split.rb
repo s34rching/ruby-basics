9d20a2461fc67d92f224ccb8f303d626bbeed451
s34rching <ivan.katsenya@gmail.com>
2020-03-15 11:49:05 +0300

    'split' method

---
+++ s10-strings-part-2/split.rb
@@ -0,0 +1,9 @@
+str = "All those mankinds will be lost in deaths like stigmas in hypnosis"
+
+arr = str.split(" ")
+arr1 = str.split # " " - default delimiter
+arr2 = str.split(" in")
+
+p arr
+p arr1
+p arr2
