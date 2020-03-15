0e9f71db2503ef05788a9d31637ba81e590b14f4
s34rching <ivan.katsenya@gmail.com>
2020-03-15 12:08:30 +0300

    Iterate over string chars

---
+++ s10-strings-part-2/iterate_over_characters.rb
@@ -0,0 +1,6 @@
+alphabetical = "Shame happens when you yearn living so spiritually that whatsoever you are shining is your afterlife"
+
+alphabetical.each_char { |char| p char }
+p alphabetical.split("")
+p alphabetical.chars
+
