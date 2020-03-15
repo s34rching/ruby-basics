df9a8614b90b827a92cc4f24bb6348a36c893fea
s34rching <ivan.katsenya@gmail.com>
2020-03-15 17:26:04 +0300

    'squeeze' method

---
+++ s10-strings-part-2/squeeze.rb
@@ -0,0 +1,23 @@
+# 'squeeze' replaces set of the same characters in a row with a single character
+
+sentence = "Thhhe aardvark         jummped  oover tthe fence!"
+
+p sentence.squeeze # squeezes all repetitive characters
+p sentence.squeeze("h") # squeezes only 'h'
+p sentence.squeeze("mh") # squeezes 'h' and 'm'
+
+p sentence
+sentence.squeeze!("t") # mutates string
+p sentence
+
+# Exercise
+
+def custom_squeeze(str)
+  squeezed = []
+
+  str.clone.split("").each { |char| squeezed << char unless squeezed.last == char }
+
+  return squeezed.join("")
+end
+
+p custom_squeeze(sentence)
