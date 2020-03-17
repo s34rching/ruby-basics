9b3b95ecd396aaee4f03a2c02210427d89183677
s34rching <ivan.katsenya@gmail.com>
2020-03-17 17:23:39 +0300

    Hash creation

---
+++ s12-hashes-part-1/creation.rb
@@ -0,0 +1,12 @@
+empty_hash = {}
+p empty_hash.class
+
+equipment = {
+  "monitors" => ["LG", "LG"],
+  "keyboard" => "Apple",
+  "mouse" => "Logitech"
+  }
+
+p equipment
+p equipment["monitors"]
+p equipment["monitors"][1]
