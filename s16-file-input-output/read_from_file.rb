5eedb57ac604501b37a82e3e9b5a3dfcbc7d4670
s34rching <ivan.katsenya@gmail.com>
2020-03-24 19:23:38 +0300

    Write file

--- s16-file-input-output/read_from_file.rb
+++ s16-file-input-output/read_from_file.rb
@@ -3,5 +3,3 @@ file_path = File.join(File.absolute_path(Dir.pwd), "#{File.dirname(__FILE__)}/fi
 File.open(file_path).each do |text|
   puts text
 end
-
-
---
+++ s16-file-input-output/write_to_file.rb
@@ -0,0 +1,18 @@
+phrase = "Wobble unearthly like a senior queen."
+phrase_1 = "Make it so, ordinary death!"
+phrase_2 = "Try crushing tart marinateed with white bread lassi, covered with dill."
+
+filename = "log-#{Time.now.strftime("%Y%m%dT%H%M%S")}.txt"
+file_path = File.join(File.absolute_path(Dir.pwd), "#{File.dirname(__FILE__)}/files/#{filename}")
+
+File.open(file_path, "w") do |file| # "w" mode overwrites content
+  file.puts(phrase)
+  file.write(phrase_1)
+  file.print(phrase_2)
+end
+
+File.open(file_path, "a") do |file| # "w" mode append new content
+  file.puts(phrase)
+  file.write(phrase_1)
+  file.print(phrase_2)
+end
