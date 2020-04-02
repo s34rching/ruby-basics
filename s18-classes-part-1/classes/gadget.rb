0d3e99577e83ab446dfd9d5ebc701100fd13108c
Ivan Katsenya <ivan.katsenya@freshly.com>
2020-04-02 10:37:31 +0300

    '.instance_variables' and '.initialize' method

--- s18-classes-part-1/classes/gadget.rb
+++ s18-classes-part-1/classes/gadget.rb
@@ -1,13 +1,15 @@
 class Gadget
-
+  def initialize
+    @username = "User #{rand(1..1000)}"
+    @password = "topsecret"
+    @production_number = "#{rand(325..1254)}-#{rand(744..15555)}-#{rand(7897..1215456)}"
+  end
 end
 
-shiny = Gadget.new
-flashy = Gadget.new
+phone = Gadget.new
+laptop = Gadget.new
 
-p shiny.object_id == flashy.object_id # false
-
-glossy = shiny
-
-p glossy == shiny # true
-p glossy.object_id == shiny.object_id # true
+p phone # => #<Gadget:0x00007feb6306ee00 @username="User 879", @password="topsecret", @production_number="966-15475-1116880">
+p phone.instance_variables # => [:@username, :@password, :@production_number]
+p laptop # => #<Gadget:0x00007feb6306ebf8 @username="User 158", @password="topsecret", @production_number="654-6563-410248">
+p laptop.instance_variables # => [:@username, :@password, :@production_number]
