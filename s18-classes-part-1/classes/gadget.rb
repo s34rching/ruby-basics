26e92f732988fc3957a6e8cd0ab7529752bdae21
Ivan Katsenya <ivan.katsenya@freshly.com>
2020-04-02 10:59:27 +0300

    'self' keyword

--- s18-classes-part-1/classes/gadget.rb
+++ s18-classes-part-1/classes/gadget.rb
@@ -6,15 +6,9 @@ class Gadget
   end
 
   def to_s
-    "Gadget '#{@production_number}' belongs to '#{@username}'"
+    "Gadget '#{@production_number}' belongs to '#{@username}'. It is made from #{self.class}. ID - #{self.object_id}"
   end
 end
 
 phone = Gadget.new
-
-# Initial version of '.to_s' representation
-# p phone.to_s # => "#<Gadget:0x00007ff7fb82f870>"
-
-# Overwritten '.to_s'
-p phone.to_s # => "Gadget '897-11703-595606' belongs to 'User 967'"
-p 5.to_s # => "5"
+p phone.to_s # => "Gadget '646-4514-1013319' belongs to 'User 313'. It is made from the Gadget. ID - 70113126660360"
