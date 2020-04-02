b4bb3d1ceaebe68f1a69b9033e4dc7360fc233f1
Ivan Katsenya <ivan.katsenya@freshly.com>
2020-04-02 10:47:33 +0300

    Instance methods

--- s18-classes-part-1/classes/gadget.rb
+++ s18-classes-part-1/classes/gadget.rb
@@ -4,12 +4,16 @@ class Gadget
     @password = "topsecret"
     @production_number = "#{rand(325..1254)}-#{rand(744..15555)}-#{rand(7897..1215456)}"
   end
+
+  def info
+    "Gadget '#{@production_number}' belongs to '#{@username}'"
+  end
 end
 
 phone = Gadget.new
 laptop = Gadget.new
 
-p phone # => #<Gadget:0x00007feb6306ee00 @username="User 879", @password="topsecret", @production_number="966-15475-1116880">
-p phone.instance_variables # => [:@username, :@password, :@production_number]
-p laptop # => #<Gadget:0x00007feb6306ebf8 @username="User 158", @password="topsecret", @production_number="654-6563-410248">
-p laptop.instance_variables # => [:@username, :@password, :@production_number]
+p phone.info
+p laptop.info
+
+p phone.methods.sort - Object.methods # => [:info]
