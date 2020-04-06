b142385b8d2d75e0eddb4e42c79057356fd6854d
Ivan Katsenya <ivan.katsenya@freshly.com>
2020-04-06 20:53:47 +0300

    Check inheritance hierarchy

--- s21-classes-part-3/subclasses/manager.rb
+++ s21-classes-part-3/subclasses/manager.rb
@@ -1,7 +1,16 @@
 require_relative "employee"
+require_relative "worker"
 
 class Manager < Employee
 end
 
 Dan = Manager.new("Dan", 35)
 p Dan.introduce
+
+p Manager.superclass
+p Manager < Employee # => true / checks inheritance
+p Manager < Kernel # => true / checks inheritance
+p BasicObject < Manager # => false / checks inheritance
+
+p Manager.ancestors
+p Worker.ancestors
