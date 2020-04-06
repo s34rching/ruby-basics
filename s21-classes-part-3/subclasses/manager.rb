2fe0a1f937c3f7910158e020a3c79a8f99810fed
Ivan Katsenya <ivan.katsenya@freshly.com>
2020-04-06 21:04:05 +0300

    '.is_a?' and '.instance_of?' predicate methods

--- s21-classes-part-3/subclasses/manager.rb
+++ s21-classes-part-3/subclasses/manager.rb
@@ -1,16 +1,6 @@
 require_relative "employee"
-require_relative "worker"
 
 class Manager < Employee
 end
 
 Dan = Manager.new("Dan", 35)
-p Dan.introduce
-
-p Manager.superclass
-p Manager < Employee # => true / checks inheritance
-p Manager < Kernel # => true / checks inheritance
-p BasicObject < Manager # => false / checks inheritance
-
-p Manager.ancestors
-p Worker.ancestors
--- s21-classes-part-3/subclasses/worker.rb
+++ s21-classes-part-3/subclasses/worker.rb
@@ -1,7 +1,22 @@
+require_relative "manager"
 require_relative "employee"
 
 class Worker < Employee
 end
 
-John = Worker.new("John", 50)
-p John.introduce
+bob = Manager.new("Bob", 48)
+daniel = Worker.new("Daniel", 36)
+
+p bob
+
+# is_a? checks in a tree
+p bob.is_a?(Worker) # => false
+p bob.is_a?(Manager) # => true
+p bob.is_a?(Employee) # => true
+p bob.is_a?(Object) # => true
+
+# instance_of checks class object inherits from
+p bob.instance_of?(Worker) # => false
+p bob.instance_of?(Manager) # => true
+p bob.instance_of?(Employee) # => false
+p bob.instance_of?(Object) # => false
